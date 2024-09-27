c17f1ac83e98d8fc57fba85290bbf6e0bffdd3dc
root <root@WIN-1J6F5NF0R04>
2024-09-28 01:47:59 +0300

    fix kraft compose

--- easy/admin.py
+++ easy/admin.py
@@ -1,4 +1,7 @@
+import asyncio
 from aiokafka.admin import AIOKafkaAdminClient, NewTopic
+from kafka.admin import KafkaAdminClient, NewTopic as OldNewTopic
+
 
 
 
@@ -9,5 +12,26 @@ async def start():
         sasl_mechanism='PLAIN',
         sasl_plain_username='admin',
         sasl_plain_password='admin_password',
+        api_version='2.4.0'
     )
-    await admin.create_topic(NewTopic(name='TEST',num_partitions=1,replication_factor=1))
+    await admin.create_topics(
+        [NewTopic(name='TEST',num_partitions=1,replication_factor=1)]
+    )
+
+
+def test():
+    admin = KafkaAdminClient(
+        bootstrap_servers=['localhost:9092','localhost:9093','localhost:9094'],
+        security_protocol='SASL_PLAINTEXT',
+        sasl_mechanism='PLAIN',
+        sasl_plain_username='admin',
+        sasl_plain_password='admin_password',
+        api_version=(2,4,0)
+    )
+
+    admin.create_topics([
+        OldNewTopic(name='TEST',num_partitions=1,replication_factor=1)
+        ])
+
+test()
+#asyncio.run(start())
